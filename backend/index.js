82e3a61773c11665abf1a2395e70c2f2669aef52
Avnish Bharadva <avnishbharadva@gmail.com>
2022-07-07 20:54:02 +0530

    Users API Created

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(express.json());   // this middleware is used for req.body
 // available routes
 
 app.use('/api/category',require('./routes/category'));
+app.use('/api/auth',require('./routes/auth'));
 
 app.listen(port, () => {
   console.log(`Coders-Forum app listening on port ${port}`)
