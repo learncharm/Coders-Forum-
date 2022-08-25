679a992c85421d119a786a86d7fa509d138adf01
Avnish Bharadva <avnishbharadva@gmail.com>
2022-08-25 16:57:29 +0530

    Added Username and Date on Thread & Comment

--- src/App.js
+++ src/App.js
@@ -20,41 +20,47 @@ import { useEffect } from 'react';
 function App() {
   useEffect(() => {
     alanBtn({
-        key: 'd58468224e69996e980b39e72a128c432e956eca572e1d8b807a3e2338fdd0dc/stage',
-        onCommand: (commandData) => {
-          if (commandData.command === 'adminPanel') {
-            // Call the client code that will react to the received command
-            window.location.href = '/admin';
-          }
-          if (commandData.command === 'homePage') {
-            // Call the client code that will react to the received command
-            window.location.href = '/';
-          }
+      key: 'd58468224e69996e980b39e72a128c432e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: (commandData) => {
+        if (commandData.command === 'adminPanel') {
+          // Call the client code that will react to the received command
+          window.location.href = '/admin';
         }
+        if (commandData.command === 'homePage') {
+          // Call the client code that will react to the received command
+          window.location.href = '/';
+        }
+      }
     });
   }, []);
   return (
     <>
-    <CategoryState>
-    <div class="alan-btn"></div>
+      <CategoryState>
+        <div class="alan-btn"></div>
 
-<BrowserRouter>
-    <NavBar/>
-    <Routes>
-      <Route path='/' element={ <Home/>}></Route>
-      <Route path='/admin' element={<Admin/>}></Route>
-      <Route path='/admin/category' element={<Category/>}></Route>
-      <Route path='/threads/:category' element={<Thread/>}></Route>
-      <Route path='/thread/:threadid' element={<Pthread/>}></Route>
-      <Route path='/admin/thread' element={<AdminThread/>}></Route>
-      <Route path='/admin/comment' element={<AdminComment/>}></Route>
-    </Routes>
+        <BrowserRouter>
+        <NavBar />
+          <Routes>
+            <Route path='/admin' element={<Admin />}></Route>
+            <Route path='/admin/category' element={<Category />}></Route>
+            <Route path='/admin/thread' element={<AdminThread />}></Route>
+            <Route path='/admin/comment' element={<AdminComment />}></Route>
+          </Routes>
+          
+          <Routes>
+          
+            <Route path='/' element={<Home />}></Route>
 
-</BrowserRouter>
+            <Route path='/threads/:category' element={<Thread />}></Route>
+            <Route path='/thread/:threadid' element={<Pthread />}></Route>
 
-</CategoryState>
-  
-  </>
+          </Routes>
+
+        </BrowserRouter>
+
+      </CategoryState>
+
+    </>
   );
 }
 
--- src/components/Thread/ThreadDetails.js
+++ src/components/Thread/ThreadDetails.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import './CardDetails.css';
 import { Link } from 'react-router-dom';
 
 export default function ThreadDetails(props) {
-    const {threads} = props;
+  const { threads } = props;
+
+  const [user, setUser] = useState([]);
+  const getUser = async () => {
+    //API Call
+    const url = `http://localhost:5000/api/auth/fetchalluser`;
+    // console.log(url)
+    const response = await fetch(url);
+    const json = await response.json();
+    // console.log(json);
+    setUser(json);
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="col-md-4 mt-3">
-         
-        <div className="card cat-card" style={{ "width": "18rem" }}>
-                        <div className="card-body m-3">
-                            <Link to={`/thread/${threads._id}`} className="card-title">{threads.title}</Link>
-                            <h6 className="card-subtitle mb-2 text-muted">&nbsp;</h6>
-                            <p className="card-text">{threads.description}</p>
-                            {/* <Link to={`/thread/${categories.title}`} class="card-link btn btn-primary">Visit Category</Link> */}
-                        </div>
-                    </div>
+
+      <div className="card cat-card" style={{ "width": "18rem" }}>
+        <div className="card-body m-3">
+          <Link to={`/thread/${threads._id}`} className="card-title">{threads.title}</Link>
+          <h6 className="card-subtitle mb-2 text-muted">&nbsp;</h6>
+          <p className="card-text">{threads.description}</p>
+          <p className="card-text">{threads.date}</p>
+
+          {user.map((users) => {
+            if (users._id == threads.userid)
+              return <p>By {users.name}</p>
+          })}
+
+          {/* <Link to={`/thread/${categories.title}`} class="card-link btn btn-primary">Visit Category</Link> */}
+        </div>
+      </div>
     </div>
   )
 }
